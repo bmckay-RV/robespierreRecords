63923634a263efdc87cfd3946b60b6527fb7f018
bmckay-RV <bmckay@redventures.com>
2021-01-16 14:48:12 -0500

    initialized db, dockerfile

---
+++ go-docker/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "robiespierre_records_db"
+)
+
+type Product struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
+
+
+
+CREATE TABLE products (
+	id SERIAL PRIMARY KEY,
+	name TEXT
+  );
+  
